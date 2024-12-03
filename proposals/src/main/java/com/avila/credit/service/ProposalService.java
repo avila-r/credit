8bd3c91706c5126374880b67a4255fa85c593c38
r. ávila <avila.dev@outlook.com>
2024-12-02 22:48:19 -0300

    feat: implement scheduler

--- proposals/src/main/java/com/avila/credit/service/ProposalService.java
+++ proposals/src/main/java/com/avila/credit/service/ProposalService.java
@@ -3,32 +3,55 @@ package com.avila.credit.service;
 import com.avila.credit.dto.ProposalRequest;
 import com.avila.credit.exception.users.UserAlreadyExistsException;
 import com.avila.credit.model.Proposal;
-import com.avila.credit.repository.*;
+import com.avila.credit.repository.ProposalRepository;
+import com.avila.credit.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 
 import lombok.AllArgsConstructor;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.StreamSupport;
 
 @AllArgsConstructor
 @Service public class ProposalService {
     private final UserRepository users;
     private final ProposalRepository proposals;
+    private final NotificationService notifier;
 
     @Transactional public Proposal create(ProposalRequest request) {
         if (users.existsByCpf(request.cpf()))
             throw new UserAlreadyExistsException("CPF " + request.cpf() + " already in use");
 
-        return proposals.save(request.entity());
+        Proposal proposal = proposals.save(request.entity());
+        boolean ok = notifier.notify(proposal);
+        if (!ok) {
+            proposal.setNotified(false);
+            return proposals.save(proposal);
+        }
+        return proposal;
     }
 
     @Transactional public List<Proposal> list() {
         return StreamSupport.stream(proposals.findAll().spliterator(), false)
                 .toList();
+    }
+
+    @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.SECONDS)
+    void notifyProposals() {
+        for (Proposal proposal : proposals.findAllByNotifiedIsFalse()) {
+            boolean ok = notifier.notify(proposal);
+            if (!ok) {
+                return;
+            }
+
+            proposal.setNotified(true);
+            proposals.save(proposal);
+        }
     }
 
 }
