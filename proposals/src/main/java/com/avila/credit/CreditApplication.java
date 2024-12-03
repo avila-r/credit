a59188a27bf4b075072aabe8d5559d42dac4b543
r. ávila <avila.dev@outlook.com>
2024-12-02 22:53:45 -0300

    feat: activate scheduling

--- proposals/src/main/java/com/avila/credit/CreditApplication.java
+++ proposals/src/main/java/com/avila/credit/CreditApplication.java
@@ -2,8 +2,10 @@ package com.avila.credit;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CreditApplication {
     public static void main(String[] args) {
         SpringApplication.run(CreditApplication.class, args);
