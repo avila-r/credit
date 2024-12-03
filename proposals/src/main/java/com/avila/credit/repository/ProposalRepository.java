0f86e7929886453ccc75a93f33954a64a7fc7872
r. ávila <avila.dev@outlook.com>
2024-12-02 22:53:31 -0300

    feat: implement method to find non-notified proposals

--- proposals/src/main/java/com/avila/credit/repository/ProposalRepository.java
+++ proposals/src/main/java/com/avila/credit/repository/ProposalRepository.java
@@ -4,9 +4,8 @@ import com.avila.credit.model.Proposal;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface ProposalRepository extends CrudRepository<Proposal, Long> {
-    Optional<Proposal> findByUser_Id(Long userId);
-    Optional<Proposal> findByUser_Cpf(String userCpf);
+    List<Proposal> findAllByNotifiedIsFalse();
 }
