34a6413861a06e76a9055a932e349b9d5d6ee1c5
r. ávila <avila.dev@outlook.com>
2024-12-03 21:45:24 -0300

    feat: add listener to remove unapproved proposals

---
+++ proposals/src/main/java/com/avila/proposals/listener/ApprovedProposalsListener.java
@@ -0,0 +1,21 @@
+package com.avila.proposals.listener;
+
+import com.avila.proposals.dto.ProposalResponse;
+import com.avila.proposals.service.ProposalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component class ApprovedProposalsListener {
+
+    private final ProposalService service;
+
+    @RabbitListener(queues = "${broker.queues.proposals.name}")
+    void listen(ProposalResponse proposal) {
+        if (!proposal.approved()) {
+            service.remove(proposal.id());
+        }
+    }
+
+}
